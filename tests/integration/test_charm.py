3bc44ae5132b2ba3ee3cfbe55af47b58c076999a
Erik Lönroth <erik.lonroth@gmail.com>
2026-04-24 23:16:06 +0200

    Fix tox and typing checks in CI

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -24,11 +24,11 @@ async def test_build_and_deploy(ops_test: OpsTest):
     """
     # Build and deploy charm from local source folder
     charm = await ops_test.build_charm(".")
+    model = ops_test.model
+    assert model is not None
 
     # Deploy the charm and wait for active/idle status
     await asyncio.gather(
-        ops_test.model.deploy(charm, application_name=APP_NAME),
-        ops_test.model.wait_for_idle(
-            apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
-        ),
+        model.deploy(charm, application_name=APP_NAME),
+        model.wait_for_idle(apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000),
     )
